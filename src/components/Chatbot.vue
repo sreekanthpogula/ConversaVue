3f00c93153136561b2973c23ecc032bf11c6641e
Sreekanth Pogula <sreekanth.pogula@senecaglobal.net>
2023-06-13 14:20:21 +0530

    Initial commit

--- src/components/Chatbot.vue
+++ src/components/Chatbot.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="chatbot">
     <div class="chatbox">
-      <div class="chatlog">
+      <transition-group name="fade">
         <div v-for="message in chatLog" :key="message.id">
-          <div v-if="message.isUser" class="user-message">{{ message.text }}</div>
-          <div v-else class="bot-message">{{ message.text }}</div>
+          <div
+            v-if="message.isUser"
+            class="user-message animated fadeInRight"
+            @click="toggleVisibility(message)"
+          >
+            {{ message.text }}
+          </div>
+          <div
+            v-else
+            class="bot-message animated fadeInLeft"
+            @click="toggleVisibility(message)"
+          >
+            {{ message.text }}
+          </div>
         </div>
-      </div>
+      </transition-group>
       <div class="input-container">
         <input v-model="userInput" @keyup.enter="sendMessage" type="text" placeholder="Type your message..." />
-        <button @click="sendMessage">Send</button>
+        <v-btn @click="sendMessage" color="primary">Send</v-btn>
       </div>
     </div>
   </div>
@@ -26,7 +38,7 @@ export default {
     return {
       chatLog: [],
       userInput: '',
-      apiEndpoint: 'https://api.chatgpt.com/v1/chat',
+      apiEndpoint: 'https://api.chatgpt.com/v1/chat/completions',
     };
   },
   methods: {
@@ -35,6 +47,7 @@ export default {
         id: Date.now(),
         text: this.userInput,
         isUser: true,
+        visible: true,
       };
 
       this.chatLog.push(userMessage);
@@ -56,12 +69,16 @@ export default {
           id: Date.now(),
           text: response.data.choices[0].message.content,
           isUser: false,
+          visible: true,
         };
 
         this.chatLog.push(botMessage);
       } catch (error) {
         console.error('ChatGPT API error:', error);
       }
+    },
+    toggleVisibility(message) {
+      message.visible = !message.visible;
     },
   },
 };
@@ -70,25 +87,86 @@ export default {
 <style scoped>
 .chatbot {
   /* Add your own styles here */
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  padding: 20px;
 }
 
 .chatbox {
   /* Add your own styles here */
+  max-width: 600px;
+  margin: 0 auto;
 }
 
 .chatlog {
   /* Add your own styles here */
+  margin-bottom: 20px;
 }
 
 .user-message {
   /* Add your own styles here */
+  background-color: #cce6ff;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
 }
 
 .bot-message {
   /* Add your own styles here */
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
 }
 
 .input-container {
   /* Add your own styles here */
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.animated {
+  animation-duration: 0.5s;
+}
+
+@keyframes fadeInLeft {
+  from {
+    opacity: 0;
+    transform: translateX(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes fadeInRight {
+  from {
+    opacity: 0;
+    transform: translateX(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+.fadeInLeft {
+  animation-name: fadeInLeft;
+}
+
+.fadeInRight {
+  animation-name: fadeInRight;
 }
 </style>
